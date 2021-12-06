8507ace3ea4317916730b040589efc297edba132
MorenaFischione <fischione.morena@gmail.com>
2021-12-06 10:30:09 +0100

    seeder order and faker

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+
+    protected $fillable = ['name', 'surname', 'phone', "email", "city", "zip_code", "address"];
+
     public function dishes(){
         return $this->belongsToMany('App\Models\Dish');
     }
