dc4886bdb4109060eb4a48ab8a3e6fafd27b3593
GianmarcoPerini <g.marco.perini@gmail.com>
2021-12-06 11:14:49 +0100

    refactor seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            UserSeederTable::class,
-            CategorySeedTable::class,
-        ]);
+        // $this->call(UsersTableSeeder::class);
     }
 }
