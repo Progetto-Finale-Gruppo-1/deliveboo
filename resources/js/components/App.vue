08f28c9f6cba5cf15657606b0187d1226c66bcb6
GianmarcoPerini <g.marco.perini@gmail.com>
2021-12-10 15:17:03 +0100

    type search

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -3,19 +3,18 @@
     <div class="container">
         <input type="text" v-model="input">
         <div v-for="type in types" :key="type.id" class="d-inline mx-2 input">
-            <input type="checkbox" :id="`input${type.id}`" :value="type.name" v-model="checked" @change="getCheck">
+            <input type="radio" :id="`input${type.id}`" :value="type.id" v-model="checked">
             <label :for="`input${type.id}`">{{ type.name }}</label>
         </div>
         <div class="row">
-            <div v-for="user in filterUSer" :key="user.id">
+            <div v-for="user in filterType" :key="user.id">
                 <div class="col-3">
                     <div class="card" style="width: 18rem;">
                         <img src="#" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <!-- <h5 class="d-none" v-if="user.category == null"></h5> -->
-                            <h5 class="cat_name">{{ user.category.name }}</h5>
-                            <p>{{ user.id }}</p>
-                            <p>{{ user.category_id }}</p>
+                            <h5 class="d-none" v-if="user.category == null"></h5>
+                            <p >{{ user.name }}</p>
+                            <p v-for="type in user.types" :key="type.id">{{ type.name }}</p>
                             <p class="card-text">{{ user.description }}</p>
                             <a :href="`/guest/dish/${ user.id }`" class="btn btn-primary">Go somewhere</a>
                         </div>
@@ -33,10 +32,10 @@ export default {
     name: 'App',
     data(){
         return{
-            input: '',
-            users: [],
-            types: [],
-            checked: [],
+            input: '',      // input ricerca type:text
+            users: [],      // array degli user
+            types: [],      // array dei tipi di ristorante
+            checked: '',    // array di ingredienti checcati
         }
     },
 
@@ -47,24 +46,43 @@ export default {
         typesList(){
             axios.get('/api/type').then(res => this.types = [...res.data])
         },
-        getCheck(){
-            console.log(this.checked);
-        }
+        // getCheck(){
+        //     console.log(this.checked);
+        // }
     },
 
     computed: {
-        filterUSer(){
+        // filterUser(){
+        //     this.users.filter((user) => {
+        //         return user.types.filter(type => {
+        //             return type.name == 'Mexican'
+        //         })
+        //     })
+        //     console.log(this.users);
+        // },
+
+
+        filterUser: function(){
             return this.users.filter((user) => {
-                if(user.category !== null) return user.category.name.toLowerCase().match(this.input.toLowerCase())
                 if(user.category !== null) return user.category.name.toLowerCase().match(this.input.toLowerCase())
             })
         },
+        filterType(){
+            let fillato = []
+            this.users.forEach(user => {
+                return user.types.forEach(type => {
+                    if(type.id == this.checked) return fillato.push(user);
+                })
+            })
+            return fillato
+        }
+
     },
 
     mounted(){
         this.usersList()
         this.typesList()
-        this.getCheck()
+        // console.log(this.filterType());
     },
     
 }
