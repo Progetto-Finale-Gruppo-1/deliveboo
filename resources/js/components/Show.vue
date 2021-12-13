ce8d790801e1fde834752bba8e67bac241a46a14
MascarBone <simone.bonelli@gmail.com>
2021-12-13 12:54:13 +0100

    using guest show blade with vue component

---
+++ resources/js/components/Show.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <h2>Titolo randomico</h2>
+      <p>Informazione passata tramite Prop: {{dato}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Show',
+    props: ['dato'],
+
+    mounted() {
+        console.log('in Prova.vue');
+        console.log(typeof this.dato);
+    }
+}
+</script>
+
+<style>
+
+</style>
